f6e77e710f0ffabff228bfb08b1dc543c288ea88
leral3 <leral3@naver.com>
2022-03-08 02:12:12 +0900

    버튼 추가

--- calc.py
+++ calc.py
@@ -13,13 +13,25 @@ calc.geometry("300x300")
 
 
 # 엔터키 연결
-def func(event):                    # func 함수 작성
-    # 계산하기
+# 버튼 추가 ( = , C) 버튼
+def calculate(event):                    # func 함수 작성 / func 함수 이름을 calculate로 변경
+    value = tk.Entry.get(display)
+    
+    if value != '':
+        result = eval(value)
+        print(result)
+        display.delete(0, tk.END)
+        display.insert(0, result)
+
+def clear(event):                        # C 버튼과 Esc 키을 위한 함수 입니다. 
+    display.delete(0, tk.END)            # 내용 삭제 
+    
+    """# 계산하기
     result = eval(tk.Entry.get(display))  # eval() 함수로 계산
     print(result)
     display.delete(0, tk.END)       # 0번째 자리부터 끝까지 삭제하는 명령어 / 내용 삭제 의미
     display.insert(0, result)       # 0번째 자리에 result라는 변수값을 입력하는 명령어 
-    """# 입력창의 값 가져오기 
+    # 입력창의 값 가져오기 
     print(tk.Entry.get(display))    # 입력창에 들어 있는 값을 출력해줍니다."""
 
 
@@ -29,7 +41,20 @@ display = tk.Entry(calc, width = 20) # display라는 이름의 출력창 추가,
 display.pack()                       # 위치를 정해주는 명령어  => 그럼 다른 위치 관련 명령어는?
 
 
-calc.bind('<Return>', func)          # 엔터키(이벤트)를 func 함수로 연결
+# 버튼 추가 
 
+button_e = tk.Button(calc, text = '=', width =5)    # '=' 버튼 추가 
+button_e.bind('<Button-1>', calculate)               # 버튼에 클릭 이벤트 추가 
+button_e.pack()
+
+
+button_c = tk.Button(calc, text = 'C', width= 5)    # 'C' 버튼 추가, text 속성은 버튼에 표시할 문자입니다.
+button_c.bind('<Button-1>', clear)                  # <Button-1> 이벤트는 마우스 왼쪽 클릭 이벤트입니다. 
+button_c.pack()
+
+
+
+calc.bind('<Return>', calculate)          # 엔터키(이벤트)를 calculate 함수로 연결
+calc.bind('<Escape>', clear)              # Esc 키도 c버튼과 동일한 기능을 하도록 연결합니다. 
 
 calc. mainloop()
